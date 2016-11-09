0729126c62b29824cb8389e516577d27ebc759e0
Joshua <jysewhee@mail.lipscomb.edu>
2016-11-08 19:24:16 -0600

    Added FamousPainting.java

---
+++ FamousPainting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Joshua Sew-Hee on 11/8/2016.
+ */
+public class FamousPainting extends Painting{
+    public FamousPainting(String t, String a){
+        super(t,a);
+        value = 25000;
+    }
+}
